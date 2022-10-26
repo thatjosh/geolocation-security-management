0351584a62849f5d7ee6efbe9a7b33ae5bf2436a
Josh <learningcs1711@gmail.com>
2022-10-26 14:08:26 +0100

    firebase env config

--- src/common/utils/firebase.ts
+++ src/common/utils/firebase.ts
@@ -1,19 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD37jbNXJ4kp3T6BMamC5C8zqg0UEjXi-Q",
-  authDomain: "geolocation-core-service.firebaseapp.com",
-  databaseURL:
-    "https://geolocation-core-service-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "geolocation-core-service",
-  storageBucket: "geolocation-core-service.appspot.com",
-  messagingSenderId: "175859049558",
-  appId: "1:175859049558:web:223ae41e7ce849b7af47f7",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  databaseURL: import.meta.env.VITE_databaseURL,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
